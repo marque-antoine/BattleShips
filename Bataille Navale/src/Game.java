e5698b752e77f36449bb8c2171d65cc701d90339
Marc-Antoine DANNER <dannerma@gmail.com>
2018-04-25 16:09:21 +0200

    M-A v1

--- Bataille Navale/src/Game.java
+++ Bataille Navale/src/Game.java
@@ -1,6 +1,10 @@
 
 public class Game {
-
+	private Player gameWinner;
+	private Player gameTurn;
+	private Player playerOne;
+	private Player playerTwo;
+	
 	public Game() {
 		// TODO Auto-generated constructor stub
 	}
@@ -10,8 +14,64 @@ public class Game {
 
 	}
 	
-	public class Game() {
+	public void start() {
 		
+	}
+
+	/**
+	 * @return the gameWinner
+	 */
+	public Player getGameWinner() {
+		return gameWinner;
+	}
+
+	/**
+	 * @param gameWinner the gameWinner to set
+	 */
+	public void setGameWinner(Player gameWinner) {
+		this.gameWinner = gameWinner;
+	}
+
+	/**
+	 * @return the gameTurn
+	 */
+	public Player getGameTurn() {
+		return gameTurn;
+	}
+
+	/**
+	 * @param gameTurn the gameTurn to set
+	 */
+	public void setGameTurn(Player gameTurn) {
+		this.gameTurn = gameTurn;
+	}
+
+	/**
+	 * @return the playerOne
+	 */
+	public Player getPlayerOne() {
+		return playerOne;
+	}
+
+	/**
+	 * @param playerOne the playerOne to set
+	 */
+	public void setPlayerOne(Player playerOne) {
+		this.playerOne = playerOne;
+	}
+
+	/**
+	 * @return the playerTwo
+	 */
+	public Player getPlayerTwo() {
+		return playerTwo;
+	}
+
+	/**
+	 * @param playerTwo the playerTwo to set
+	 */
+	public void setPlayerTwo(Player playerTwo) {
+		this.playerTwo = playerTwo;
 	}
 
 }
---
+++ Bataille Navale/src/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author marc-antoine
+ *
+ */
+public class Main {
+	
+
+	/**
+	 * 
+	 */
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Game game = new Game();
+        game.start();
+	}
+
+}
